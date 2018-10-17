f0954200d293f48127435cb0b2bb01201e33b789
Marat Tukhvatullin <marat.tukhvatullin@freshcells.de>
2018-10-17 15:42:36 +0200

    Updates

---
+++ precache-manifest.8e0ea53d3aa47392748a1812c63f2f1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8cbc6165a2ec55aab023",
+    "url": "/static/js/main.8cbc6165.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "8cbc6165a2ec55aab023",
+    "url": "/static/css/main.70253b99.chunk.css"
+  },
+  {
+    "revision": "e275955494a5aedcf62f18f164ca3b3d",
+    "url": "/index.html"
+  }
+];
